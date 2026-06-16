cbfe2ce600c51641cbf463e6b9188e702b6730ff
Yugawathi123 <yugawathi2007@gmail.com>
2026-06-16 19:08:57 +0530

    Add files via upload

---
+++ domain-c-ai-inference/Benchmarks/tensorrt_int8/int8_trt_benchmark.py
@@ -0,0 +1,30 @@
+from ultralytics import YOLO
+import time
+import numpy as np
+import nvtx
+
+model = YOLO("models/weights/best_int8.engine")
+
+dummy = np.random.randint(
+    0, 255,
+    (640, 640, 3),
+    dtype=np.uint8
+)
+
+# Warmup
+for _ in range(20):
+   nvtx.push_range("TensorRT_INT8")
+model(dummy, verbose=False)
+nvtx.pop_range()
+times = []
+
+for _ in range(100):
+    t0 = time.perf_counter()
+    model(dummy, verbose=False)
+    times.append((time.perf_counter() - t0) * 1000)
+
+latency = np.mean(times)
+fps = 1000 / latency
+
+print(f"Latency: {latency:.2f} ms")
+print(f"FPS: {fps:.2f}")
